f859144030ec69365b7efb243cccf77418946628
Sayed Jilliyan <netslayer71@gmail.com>
2025-07-22 05:51:38 +0700

    update payment 3

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,10 @@ io.on('connection', (socket) => {
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ['https://radiantrage.vercel.app'], // ganti dengan domain frontend kamu
+  credentials: true // jika pakai cookies/auth
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
