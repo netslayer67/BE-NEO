7591c0a783c2333b67a02e2dfd904ac7b600b2f7
Sayed Jilliyan <netslayer71@gmail.com>
2025-07-22 05:55:48 +0700

    update payment 3

--- src/server.ts
+++ src/server.ts
@@ -36,10 +36,14 @@ io.on('connection', (socket) => {
 });
 
 // Middlewares
-app.use(cors({
-  origin: ['https://radiantrage.vercel.app'], // ganti dengan domain frontend kamu
-  credentials: true // jika pakai cookies/auth
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://radiantrage.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
